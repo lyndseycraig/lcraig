1b3a247ec6e903bd9d78c1f2bd430e76c1b52828
Lyndsey Craig <lcraig@csumb.edu>
2017-10-15 21:43:48 +0000

    lab5 v2

--- dbConnection.php
+++ dbConnection.php
@@ -1,9 +1,9 @@
 <?php
 function getDatabaseConnection(){
-$host = 'localhost';
-$dbname = 'quotes';
-$username = 'root';
-$password = '';
+$host = 'us-cdbr-iron-east-05.cleardb.net';
+$dbname = 'heroku_af87a74c6e99823';
+$username = 'b2062ab40b269a';
+$password = '96726403';
 //creates database connection
 $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
