e8f4abf0ed02a04f950b78ac0aee5d7d66a422e8
Lyndsey Craig <lcraig@csumb.edu>
2017-12-03 23:26:00 +0000

    lab10

--- labs/lab10/index.php
+++ labs/lab10/index.php
@@ -5,23 +5,23 @@ function displayImages(){
     //print_r($_FILES);
     
     //echo $_FILES['myFile']['name'];
-    if(filesize($_FILES['myFile']['tmp_name'])<1000000){
-        move_uploaded_file($_FILES['myFile']['tmp_name'], "gallery/". $_FILES['myFile']['name']);
-        
-        //echo "<img src='gallery/" . $_FILES['myFile']['name'] . "'>";
-        
-        $files=scandir("gallery/", 1);
-         
-        print_r($files);  
-        
-        for($i=0; $i<count($files)-2; $i++){
-            echo "<img src='gallery/" . $files[$i] . "'id='image' onclick='imageSize()' width='35'><br>";
+        if($_FILES['myFile']['size']<1000000){
+            move_uploaded_file($_FILES['myFile']['tmp_name'], "gallery/". $_FILES['myFile']['name']);
+            
+            //echo "<img src='gallery/" . $_FILES['myFile']['name'] . "'>";
+            
+            $files=scandir("gallery/", 1);
+             
+            //print_r($files);  
+            
+            for($i=0; $i<count($files)-2; $i++){
+                echo "<img src='gallery/" . $files[$i] . "'id='image". $i ."'onclick='imageSize(".$i.")' width='35'>";
+            }
         }
-    }
-    else{
-        
-        echo "Your file is too large. Please upload a file under 1MB";
-    }
+        else{
+            
+            echo "Your file is too large. Please upload a file under 1MB";
+        }
     }
     
     
@@ -36,13 +36,14 @@ function displayImages(){
     <head>
         <title>Lab 10: File Upload </title>
          <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-        
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lily+Script+One|Roboto" rel="stylesheet"> 
         <script>
             function imageSize(i){
-                document.getElementById("image").style.width = "300px";
-                document.getElementById("image").style.height = "auto";
+                document.getElementById("image"+i).style.width = "300px";
+                document.getElementById("image"+i).style.height = "auto";
                 
-                $(document)
+                
             }
         </script>
     </head>
@@ -53,7 +54,7 @@ function displayImages(){
         <form method="POST" enctype="multipart/form-data">
             Upload file:
             
-            <input type="file" name="myFile"/>
+            <input type="file" name="myFile"/><br><br>
             <input type="submit" name="submitForm" value="Upload"/>
             
             
